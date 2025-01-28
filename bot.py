ac351bcc64c7a85a3d26acd40c927cde5fec4671
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2025-01-27 23:59:47 -0500

    Dont Question my methods

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+import ollama
 
 
 def run_bot():
@@ -11,18 +12,34 @@ def run_bot():
     intents = discord.Intents.default()
     client = discord.Client(intents=intents)
     tree = app_commands.CommandTree(client)
+    clients = ollama.Client()
 
     @client.event
     async def on_ready():
         print("Bot is running")
         try:
             await tree.sync()
+            print("Synced")
         except Exception as e:
+            await tree.sync()
             print(e)
 
-    @tree.command(name="hello")
-    async def hello(interaction: discord.Integration):
-        await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!")
+    def split_string_every_2000_chars(input_string):
+        return [input_string[i:i + 2000] for i in range(0, len(input_string), 2000)]
+    @tree.command(name="google", description="ai chatbot that can write code.")
+    async def google(interaction: discord.Integration, prompt: str):
+        model = "deepseek-r1:8b"
+        await interaction.response.defer()
+        try:
+            response = clients.generate(model=model, prompt=prompt)
+            input_string = response.response  # replace with your string
+            chunks = split_string_every_2000_chars(input_string)
+            await interaction.followup.send(f"Prompt: {prompt}. \n Response:")
+            for chunk in chunks:
+                await interaction.followup.send(f"{chunk}")
+            # await interaction.followup.send(f"Prompt: {prompt}. \n Response: {response.response}")
+        except Exception as e:
+            print("Error occurred: ", e)
 
     @tree.command(name="clear")
     async def clear(ctx: commands.Context, amount: int):
